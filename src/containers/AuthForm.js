1fe28c198e66f574f8e60977a678e072f9c0a939
Gustavo Morales <gustavo.morales@gmail.com>
2021-07-15 19:37:34 -0700

    Add Faker and Tests

--- src/containers/AuthForm.js
+++ src/containers/AuthForm.js
@@ -1,20 +1,23 @@
 import { Button, TextField, Typography } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import API from '../api';
 import { setSession } from '../utils/auth';
 import { useStore } from '../store/Store';
+import Alert from '../components/Alert';
 
 export default function AuthForm() {
   const history = useHistory();
   const {
     actions: { login },
   } = useStore();
+  const [error, setError] = useState('');
 
   async function onSubmit(event) {
     event.preventDefault();
     const { username, password } = event.target.elements;
     try {
+      setError('');
       const data = await API.login({
         username: username.value,
         password: password.value,
@@ -29,15 +32,17 @@ export default function AuthForm() {
       });
       history.push('/');
     } catch (error) {
-      console.error(error);
+      setError('Incorrect username or password');
     }
   }
 
   return (
     <>
       <Typography variant="h4">Login</Typography>
+      {error && <Alert severity="error" message={error} />}
       <form onSubmit={onSubmit}>
         <TextField
+          placeholder="username"
           name="username"
           label="Username"
           variant="outlined"
@@ -45,6 +50,7 @@ export default function AuthForm() {
           fullWidth
         />
         <TextField
+          placeholder="password"
           name="password"
           label="Password"
           type="password"
@@ -53,6 +59,7 @@ export default function AuthForm() {
           fullWidth
         />
         <Button
+          placeholder="submit"
           variant="contained"
           color="primary"
           margin="normal"
